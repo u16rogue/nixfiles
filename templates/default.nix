2862cbbb07619e8801775a06efa9f151886e8611
u16rogue <u16rogue@ushort.moe>
2026-06-17 16:14:26 +0000

    added description to template entries for `nix flake check`

--- templates/default.nix
+++ templates/default.nix
@@ -4,6 +4,7 @@ in lib.pipe (builtins.readDir ./.) [
     (lib.filterAttrs (name: value: value == "directory"))
     (lib.mapAttrs (name: value: {
         path = ./. + "/${name}"; # sloppa told me this is the right thing to do
+        description = ""; # just to satisfy nix flake check
     }))
 ]
 
